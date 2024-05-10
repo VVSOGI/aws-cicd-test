e31f05a1cacfbbd9f334806f50ea603b8fe4e5b6
benny <vvsogi@gmail.com>
2024-05-10 18:58:30 +0900

    Refactor board service

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { Board } from './entities/boards.entity';
 import { BoardsRepository } from './boards.repository';
 import { AuthService } from 'src/auth/auth.service';
 import { CreateBoard, GetBoards } from './type/types';
@@ -11,6 +12,37 @@ export class BoardsService {
     private boardsRepository: BoardsRepository,
     private authService: AuthService,
   ) {}
+
+  private async addEtcBoardData(board: Board) {
+    const profile = await this.authService.profile(board.userId);
+    const imageUrl = `${process.env.AWS_S3_URL}${board.imagePath}`;
+
+    if (!board.imagePath) {
+      return {
+        ...board,
+        nickname: profile.nickname,
+        profileImage: profile.profileImage,
+      };
+    }
+
+    return {
+      ...board,
+      profileImage: profile.profileImage,
+      nickname: profile.nickname,
+      image: imageUrl,
+    };
+  }
+
+  private async uploadImageToS3(imagePath: string, imageBuffer: Buffer) {
+    const params = {
+      Bucket: process.env.AWS_S3_BUCKET,
+      Key: imagePath,
+      Body: imageBuffer,
+    };
+
+    await s3.upload(params).promise();
+  }
+
   /**
    * 프론트 측에서 element가 하나인 activityDate, activityTime를 보낼 때,
    * 문자열 타입으로 들어오는데 이를 프론트에서 해결할 수 없어서 서버에서 처리함
@@ -34,59 +66,28 @@ export class BoardsService {
 
   async getAllBoards(getBoards: GetBoards) {
     const { data, total } = await this.boardsRepository.getAllBoards(getBoards);
-    const addImageBoards = data.map((board) => {
-      if (!board.imagePath) {
-        return board;
-      }
-      const image = process.env.AWS_S3_URL + board.imagePath;
-      return {
-        ...board,
-        image: image,
-      };
-    });
-    return { data: addImageBoards, total };
+    const boards = await Promise.all(
+      data.map(async (board) => {
+        const contents = await this.addEtcBoardData(board);
+        return contents;
+      }),
+    );
+    return { data: boards, total };
   }
 
   async getBoardById(id: string) {
-    const board = await this.boardsRepository.getBoardById(id);
-    const profile = await this.authService.profile(board.userId);
-    const image = process.env.AWS_S3_URL + board.imagePath;
-
-    if (!board.imagePath) {
-      return {
-        data: {
-          ...board,
-          nickname: profile.nickname,
-        },
-      };
-    }
-
+    const findBoard = await this.boardsRepository.getBoardById(id);
+    const board = await this.addEtcBoardData(findBoard);
     return {
-      data: {
-        ...board,
-        nickname: profile.nickname,
-        image: image,
-      },
+      data: board,
     };
-  }
-
-  private async uploadImageToS3(imagePath: string, imageBuffer: Buffer) {
-    const params = {
-      Bucket: process.env.AWS_S3_BUCKET,
-      Key: imagePath,
-      Body: imageBuffer,
-    };
-
-    await s3.upload(params).promise();
   }
 
   async uploadImage(
     file: Express.Multer.File,
     userId: string,
   ): Promise<string> {
-    if (!file) {
-      return '';
-    }
+    if (!file) return '';
     const imageId = v4();
     const filenames = file.originalname.split('.');
     const extension = filenames[filenames.length - 1];
@@ -97,18 +98,14 @@ export class BoardsService {
 
   async searchAddress(keyword: string) {
     const data = await this.boardsRepository.searchAddress(keyword);
-    const addImageBoards = data.map((board) => {
-      if (!board.imagePath) {
-        return board;
-      }
-      const image = process.env.AWS_S3_URL + board.imagePath;
-      return {
-        ...board,
-        image: image,
-      };
-    });
+    const boards = await Promise.all(
+      data.map(async (board) => {
+        const contents = await this.addEtcBoardData(board);
+        return contents;
+      }),
+    );
     return {
-      data: addImageBoards,
+      data: boards,
     };
   }
 }
