75b3b82778b3f0058d739e58ee104ff740f98b99
benny <vvsogi@gmail.com>
2024-05-30 17:49:44 +0900

    Refactor auth.controller.test.sh

---
+++ test/auth.controller.test.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+set -e
+cd "$(dirname "$0")"
+source ../.env
+source ./.config
+
+# login
+login() {
+  local email=$1
+  local password=$2
+  local url=$3
+
+  local response=$(curl -X POST \
+    -H "Content-Type: application/json" \
+    -d '{"email": "'"$email"'", "password": "'"$password"'"}' \
+    "$url/auth/login")
+
+  echo "$response"
+}
+
+# profile
+get_profile() {
+  local accessToken=$1
+  local url=$2
+
+  local response=$(curl -X GET \
+    -H "Content-Type: application/json" \
+    -H "Authorization: Bearer $accessToken" \
+    "$url/auth/profile")
+
+  echo "$response"
+}
+
+# Google URL
+get_google_auth_url() {
+  local url=$1
+
+  local response=$(curl -X GET \
+    -H "Content-Type: application/json" \
+    "$url/auth/google")
+
+  echo "$response"
+}
+
+# refresh token
+refresh_token() {
+  local refreshToken=$1
+  local userId=$2
+  local url=$3
+
+  local response=$(curl -X POST \
+    -H "Content-Type: application/json" \
+    -d '{"refreshToken": "'"$refreshToken"'"}' \
+    "$url/auth/refresh/$userId")
+
+  echo "$response"
+}
